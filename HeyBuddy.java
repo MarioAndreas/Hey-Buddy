5832480ec563ac63cbc7284b4247566fbfd7886b
Mario Kaack <mario@shortrope.com>
2014-09-04 14:21:18 -0700

    Hey Buddy v1.1,  update README
    Use a String variable instead of the String literal in HeyBuddy.java
    Learded what an .md file is. Added some formatting to README.md

--- HeyBuddy.java
+++ HeyBuddy.java
@@ -1,9 +1,11 @@
 // Test file for testing git 
+// v1.1
 
 class HeyBuddy {
 
     public static void main(String[] args) {
 
-        System.out.println("Hey Buddy");
+        String str = "Hey Buddy";   // Change 1
+        System.out.println(str);
     }
 }
